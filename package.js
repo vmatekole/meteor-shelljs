3a800b3f053594c8046f19ac9ae2ec158c0630bb
vmatekole <victor@matekole.com>
2016-09-28 15:10:33 +0200

    update package name

--- package.js
+++ package.js
@@ -1,10 +1,10 @@
 Package.describe({
-    name: 'izzilab:shelljs',
+    name: 'vmatekole:shelljs',
     version: '0.0.3',
     // Brief, one-line summary of the package.
     summary: 'Portable Unix shell commands for Meteor',
     // URL to the Git repository containing the source code for this package.
-    git: 'https://github.com/mrphu3074/meteor-shelljs.git',
+    git: 'https://github.com/vmatekole/meteor-shelljs.git',
     // By default, Meteor will default to using README.md for documentation.
     // To avoid submitting documentation, set this field to null.
     documentation: 'README.md'
@@ -22,6 +22,6 @@ Package.onUse(function (api) {
 
 Package.onTest(function (api) {
     api.use('tinytest');
-    api.use('izzilab:shelljs', 'server');
+    api.use('vmatekole:shelljs', 'server');
     api.addFiles('shelljs-tests.js', 'server');
 });
